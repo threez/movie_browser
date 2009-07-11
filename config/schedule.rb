113068b4194ec807c5f67fa0968890e217e91a68
Vincent Landgraf <setcool@gmx.de>
2009-07-11 12:45:35 +0200

    added cronjob based on wheneverize

---
+++ config/schedule.rb
@@ -0,0 +1,14 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+set :path, File.dirname(File.join('..', File.dirname(__FILE__)))
+set :cron_log, "#{path}/log/cron_log.log"
+
+every 1.hours do
+  command "cd #{path} && ruby import.rb sync"
+end
+
+# Learn more: http://github.com/javan/whenever
