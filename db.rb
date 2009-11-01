f6408263083f29f3f335fef1a833d34b8839c36a
Vincent Landgraf <setcool@gmx.de>
2009-11-01 15:18:46 +0100

    changed image path

--- db.rb
+++ db.rb
@@ -24,7 +24,7 @@ class Movie < ActiveRecord::Base
   end
   
   def image_path
-    "http://www.cinebank.de/fol/LOCANDINE/#{self.movie_id}.jpg"
+    "http://www.cinebank.de/fol/locandine/#{self.movie_id}.jpg"
   end
 end
 
